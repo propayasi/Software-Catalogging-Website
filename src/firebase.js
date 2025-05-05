70a2dec2fa4bc7be5541c2505b272ad8029dd4e1
Achyut Payasi <65479589+propayasi@users.noreply.github.com>
2025-05-05 14:47:03 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCrg78XB8OqEVy9jUwFD2VzzXGeih7n_4k",
-  authDomain: "component-catalog-app.firebaseapp.com",
-  projectId: "component-catalog-app",
-  storageBucket: "component-catalog-app.firebasestorage.app",
-  messagingSenderId: "22358917026",
-  appId: "1:22358917026:web:b4424cd42e843114e3fcfb",
-  measurementId: "G-ENB2G7D18S"
+//put your API here, Refer readme.md for futher information
 };
 
 export const componentStorageName = "components";
